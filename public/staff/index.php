f9cca6599772dfd4097b775312f5829a0bd388e7
MAC-DSK <ronyortiz@MAC-DSKs-iMac.local>
2018-01-23 10:03:33 -0400

    Initial commit

---
+++ public/staff/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Pleiades STAFF</title>
+</head>
+<body>
+	<header>
+		<h1>PLM Staff Area</h1>
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Menu</a></li>
+		</ul>
+	</nav>
+
+	<footer>
+		&copy; <?php echo date('Y'); ?> Pleiades Moon
+	</footer>
+
+</body>
+</html>
